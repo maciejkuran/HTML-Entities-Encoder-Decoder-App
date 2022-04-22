63adc2b514c45af2c2dffb12c488126fc75dce8c
Maciej Kuran - Janowski <maciejkuran@gmail.com>
2022-04-22 12:04:38 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const navMenu = document.querySelector("nav");
-
-hamburgerMenu.addEventListener("click", () => {
-  navMenu.classList.toggle("showNav");
-  hamburgerMenu.classList.toggle("active");
-});
